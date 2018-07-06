5485fd425e384274b878f1f5995cc3afcc5310a6
winioleh <olegwini98@gmail.com>
2018-07-06 10:25:08 +0300

    change file location

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from settings import TOKEN
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 from settings import TOKEN
-
+import datetime
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
@@ -39,51 +39,62 @@ def handle_video(bot, update):
     barcode = r.get(update.message.from_user.id)
     if not barcode:
         update.message.reply_text("Спершу тобі потрібно сфотографувати штрихкод товару та завантажити сюди.")
-        return
     else:
+        video_directory = dir_path + '/videos/%s' % barcode.decode("utf-8")
+        if not os.path.exists(video_directory):
+            os.makedirs(video_directory)
+
+        count_of_file = len([name for name in os.listdir(video_directory)])
         video_file = bot.get_file(update.message.video.file_id)
         if update.message.video.duration < 25:
             update.message.reply_text("Відеоролік надто короткий, необхідно щоб він тривав більше 25 секунд.")
         else:
-            video_file.download(dir_path + '/videos/%s.mp4' % barcode.decode("utf-8"))
+            video_file.download(video_directory + '/%s-%s.mp4' % (barcode.decode("utf-8"), str(count_of_file+1)))
             update.message.reply_text("Відеоролік збережено.")
             update.message.reply_text("Завантажте фото штрих-коду наступного товару.")
             r.delete(update.message.from_user.id)
 
 
+def video_count(bot, update):
+    DIR = dir_path + '/videos/'
+    dirs = [name for name in os.listdir(DIR)]
+    tmp = [len([file for file in os.listdir(DIR+directory)]) for directory in dirs]
+    update.message.reply_text("Кількість відео роликів : %s." % sum(tmp) )
+
+
 def handle_photo(bot, update):
+
     update.message.reply_text("Очікуйте результату")
     photo_file = bot.get_file(update.message.photo[-1].file_id)
+    filename = dir_path+'/imgs/%s.jpg' % update.message.from_user.id
     photo_file.download(dir_path+'/imgs/%s.jpg' % update.message.from_user.id)
     decoded_barcode = decode(Image.open(dir_path + '/imgs/%s.jpg' % update.message.from_user.id))
     barcode = decoded_barcode[0].data.decode("utf-8")
     r.set(update.message.from_user.id, barcode)
     update.message.reply_text("Розпізнаний штрих-код: " + barcode+ ".")
     update.message.reply_text("Тепeр зніміть та завантажте відеоролік, який хочете асоціювати до цього штрих-кодую. Необхідно щоб він тривав більше 25 секунд")
+    try:
+        os.remove(filename)
+    except OSError:
+        pass
 
 
 def main():
-    # Create the Updater and pass it your bot's token.
     updater = Updater(TOKEN)
 
-    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("video_count", video_count))
     dp.add_handler(MessageHandler(Filters.photo, handle_photo))
     dp.add_handler(MessageHandler(Filters.video, handle_video))
 
-    # log all errors
     dp.add_error_handler(error)
 
-    # Start the Bot
     updater.start_polling()
 
-    # Block until the user presses Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+
     updater.idle()
 
 
